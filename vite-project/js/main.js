c0499ef4caf168c1f50faead1f6875bfcbd62e8c
jasminew13 <jasminewu101@gmail.com>
2023-11-13 10:12:35 -0500

    where is the commit to github

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,12 +1,11 @@
 import './style.css'
-import { setupCounter } from './counter.js'
 
-document.querySelector(".change").addEventListener("click", function(){
-  if(document.body.classList.contains("light")){
-    document.body.classList.add("dark");
-    document.body.classList.remove("light");
+document.querySelector(".btn").addEventListener("click", function(){
+  if(document.body.classList.contains(".dark")){
+    document.body.classList.add(".dark");
+    document.body.classList.remove(".light");
   } else{
-    document.body.classList.add("light")
-    document.body.classList.remove("dark")
+    document.body.classList.add(".light")
+    document.body.classList.remove(".dark")
   }
 });

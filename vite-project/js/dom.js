fb9847a704daed5c7ce83f8a1e64115ea039b7b4
jasminew13 <jasminewu101@gmail.com>
2023-11-14 10:13:30 -0500

    ere

---
+++ vite-project/js/dom.js
@@ -0,0 +1,5 @@
+const lightthings = {
+    name: "sun",
+    
+
+}
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,11 +1,9 @@
-import './style.css'
-
 document.querySelector(".btn").addEventListener("click", function(){
-  if(document.body.classList.contains(".dark")){
-    document.body.classList.add(".dark");
-    document.body.classList.remove(".light");
+  if(document.body.classList.contains("dark")){
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
   } else{
-    document.body.classList.add(".light")
-    document.body.classList.remove(".dark")
+    document.body.classList.add("light")
+    document.body.classList.remove("dark")
   }
 });

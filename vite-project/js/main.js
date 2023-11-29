dcda7e78659adc0557cffa14a67f62e4fc6a2d27
jasmine <jasminewu101@gmail.com>
2023-11-28 22:41:35 -0500

    sigh

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,9 +2,6 @@ import { albums } from '../js/albums'
 import '../css/album.css'
 import { DOMselectors } from '../js/dom'
 
-function clearfields(){
-  DOMselectors.container.innerHTML="";
-}
  document.querySelector(".theme").addEventListener("click", function(){
   if(document.body.classList.contains("dark")){
     document.body.classList.add("light");
@@ -25,20 +22,27 @@ function clearfields(){
       <h2 class="albumname"> ${s.name}</h2>
       <h2 class="artistname"> ${s.artist}</h2>
       <img src="${s.image}" alt="" class="image">
-      <h3 class="pricing">${s.price}</h3>
+      <h3 class="price">${s.price}</h3>
   </div>`
   )})};
   
-addcards(albums);
+addcards(albums)
+
+function clearscreen(){
+  const clear = document.querySelector(".container");
+  clear.innerHTML = ""
+};
 
 let buttons = document.querySelectorAll(".pop, .indiepop, .rnb")
-buttons.forEach((btn) => btn.addEventListener("click", function(){
-  let type = btn.textContent
-  let newArr = albums.filter((album) => album.genre === type)
-  clearfields();
-  addcards(newArr);
-  addcards(albums);
+
+buttons.forEach((button) => button.addEventListener("click", function(){
+  let type = button.textContent;
+  let newarr = albums.filter((album) => album.genre === type);
+  clearscreen();
+  addcards(newarr);
 }))
-/* DOMselectors.buttons.addEventListener("click", function(){
-  clearfields();
-}) */
+
+DOMselectors.container.addEventListener("click", function(){
+  clearscreen();
+  addcards(albums)
+})

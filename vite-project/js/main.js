c62500bad125707b30be6d2061394d19bf3670a9
jasminew13 <jasminewu101@gmail.com>
2023-11-30 10:15:27 -0500

    sdfa

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,17 +1,6 @@
 import { albums } from '../js/albums'
 import '../css/album.css'
 import { DOMselectors } from '../js/dom'
-
- document.querySelector(".theme").addEventListener("click", function(){
-  if(document.body.classList.contains("dark")){
-    document.body.classList.add("light");
-     document.body.classList.remove("dark");
-   } else{
-    document.body.classList.add("dark")
-    document.body.classList.remove("light")
-   }
- });
-
  /* DOMselectors.form.addEventListener('submit', function (ok) {
   ok.preventDefault(); console.log(DOMselectors)*/
  
@@ -29,20 +18,29 @@ import { DOMselectors } from '../js/dom'
 addcards(albums)
 
 function clearscreen(){
-  const clear = document.querySelector(".container");
-  clear.container.innerHTML= "";
+  DOMselectors.container.innerHTML= "";
 };
-
+clearscreen()
+ DOMselectors.container.addEventListener("click", function(){
+  clearscreen();
+  //addcards(albums)
+}); 
 let buttons = document.querySelectorAll(".pop, .indiepop, .rnb");
 
 buttons.forEach((button) => button.addEventListener("click", function(){
   let type = button.textContent;
-  let newarr = albums.filter((album) => album.genre === type);
+  let newArr = albums.filter((album) => album.genre === type);
   clearscreen();
-  addcards(newarr);
+  addcards(newArr);
 }));
 
-DOMselectors.container.addEventListener("click", function(){
-  clearscreen();
-  addcards(albums)
-});
+
+document.querySelector(".theme").addEventListener("click", function(){
+  if(document.body.classList.contains("dark")){
+    document.body.classList.add("light");
+     document.body.classList.remove("dark");
+   } else{
+    document.body.classList.add("dark")
+    document.body.classList.remove("light")
+   }
+ });
